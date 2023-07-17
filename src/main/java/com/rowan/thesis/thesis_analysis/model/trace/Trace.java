4dff31a9ac93678de5998b8bcb54e83bbeae2ff3
rowan piek <rowan.piek@teampicnic.com>
2023-07-17 11:00:19 +0200

    Revert "Added the removal of root node database calls and added support for traces with one vertex."
    This reverts commit c02e3d08c92725cd3240cecee178a4c25d1b29b4.

--- src/main/java/com/rowan/thesis/thesis_analysis/model/trace/Trace.java
+++ src/main/java/com/rowan/thesis/thesis_analysis/model/trace/Trace.java
@@ -1,9 +1,6 @@
 package com.rowan.thesis.thesis_analysis.model.trace;
 
-import com.rowan.thesis.thesis_analysis.utility.ModelConstants;
-import java.util.HashSet;
 import java.util.Set;
-import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -23,32 +20,5 @@ public class Trace {
 
     public void addEdges(Set<Edge> edges) {
         this.edges.addAll(edges);
-    }
-
-    public Vertex findRootNode() {
-        Set<Vertex> targetVertices = new HashSet<>();
-
-        // Collect all target vertices from edges
-        for (Edge edge : edges) {
-            targetVertices.add(edge.getTarget());
-        }
-
-        // Find the root node among the vertices
-        for (Vertex vertex : vertices) {
-            if (!targetVertices.contains(vertex)) {
-                return vertex;
-            }
-        }
-
-        return null; // No root node found
-    }
-
-    public void removeRootNodeDatabaseCalls() {
-        Vertex vertex = this.findRootNode();
-        Set<Edge> databaseEdges = this.edges.stream().filter(edge -> edge.getSource().equals(vertex) && edge.getTarget().getName().equals(ModelConstants.DATABASE_NAME)).collect(Collectors.toSet());
-        Set<Vertex> databaseVertices = databaseEdges.stream().map(Edge::getTarget).collect(Collectors.toSet());
-
-        this.vertices.removeAll(databaseVertices);
-        this.edges.removeAll(databaseEdges);
     }
 }
